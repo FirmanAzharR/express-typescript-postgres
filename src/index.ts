6186759c2129fe0979158897a61a2a4d54076552
FirmanAzharR <firmanazharr@gmail.com>
2023-02-14 10:27:00 +0700

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from 'express'
+
+
+class App {
+    public app: Application
+    constructor() {
+        this.app = express()
+        this.routes()
+    }
+
+    protected routes(): void {
+        this.app.route("/").get((req: Request, res: Response) => {
+            res.send('Api using express and TS')
+        })
+    }
+}
+
+const port: number = 8000
+const app = new App().app
+app.listen(port, () => {
+    console.log(`server is running`)
+})
